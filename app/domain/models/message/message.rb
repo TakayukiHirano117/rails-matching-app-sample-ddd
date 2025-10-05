968afcb33ce83727c57f694653603eceb8c427ae
TakayukiHirano117 <ikusa.hirano@icloud.com>
2025-10-05 22:25:12 +0900

    [Update][Add]メッセージの値オブジェクト・エンティティを実装

---
+++ app/domain/models/message/message.rb
@@ -0,0 +1,51 @@
+# frozen_string_literal: true
+
+class Message
+  attr_reader :message_id, :sender_user_id, :receiver_user_id, :message_content
+
+  def initialize(
+    message_id:,
+    sender_user_id:,
+    receiver_user_id:,
+    message_content:,
+  )
+    @message_id = message_id
+    @sender_user_id = sender_user_id
+    @receiver_user_id = receiver_user_id
+    @message_content = message_content
+  end
+
+  def self.new(
+    message_id:,
+    sender_user_id:,
+    receiver_user_id:,
+    message_content:,
+  )
+    message_id, err = Uuid.new(message_id)
+    return err unless err.nil?
+
+    sender_user_id, err = Uuid.new(sender_user_id)
+    return err unless err.nil?
+
+    receiver_user_id, err = Uuid.new(receiver_user_id)
+    return err unless err.nil?
+
+    message_content, err = MessageContent.new(message_content)
+    return err unless err.nil?
+
+    super(message_id:, sender_user_id:, receiver_user_id:, message_content:)
+  end
+
+  def self.create(
+    sender_user_id:,
+    receiver_user_id:,
+    message_content:,
+  )
+    new(
+      message_id: SecureRandom.uuid,
+      sender_user_id:,
+      receiver_user_id:,
+      message_content:,
+    )
+  end
+end
---
+++ app/domain/models/vo/message_content.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+class MessageContent
+  attr_reader :value
+
+  def initialize(value:)
+    @value = value
+  end
+
+  def self.new(value)
+    return nil, "メッセージ内容は必須です" if value.blank?
+    return nil, "メッセージ内容は1文字以上1000文字以内で入力してください" if value.length > 1000
+
+    super(value:)
+  end
+end
