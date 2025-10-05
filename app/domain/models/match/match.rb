80f9a757abe67c1d8f37ffd0aed28e10291fdf38
TakayukiHirano117 <ikusa.hirano@icloud.com>
2025-10-05 22:21:14 +0900

    [Update]マッチングのエンティティを実装

---
+++ app/domain/models/match/match.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+class Match
+  attr_reader :match_id, :user_id1, :user_id2
+
+  def initialize(
+    match_id:,
+    user_id1:,
+    user_id2:,
+  )
+    @match_id = match_id
+    @user_id1 = user_id1
+    @user_id2 = user_id2
+  end
+
+  def self.new(
+    match_id:,
+    user_id1:,
+    user_id2:,
+  )
+    match_id, err = Uuid.new(match_id)
+    return err unless err.nil?
+
+    user_id1, err = Uuid.new(user_id1)
+    return err unless err.nil?
+
+    user_id2, err = Uuid.new(user_id2)
+    return err unless err.nil?
+
+    super(match_id:, user_id1:, user_id2:)
+  end
+
+  def self.create(
+    user_id1:,
+    user_id2:,
+  )
+    new(
+      match_id: SecureRandom.uuid,
+      user_id1:,
+      user_id2:,
+    )
+  end
+end
